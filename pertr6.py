c5f2fb0b02f1a1d74e407a44fcbaf8a031cb0fa8
Rasyid-Saputra <147676432+Rasyid-Saputra@users.noreply.github.com>
2024-05-16 13:59:40 +0800

    Add files via upload

---
+++ pertr6.py
@@ -0,0 +1,38 @@
+#Perbandingan
+#lebih dari >
+#kurang dari <
+
+
+
+
+
+
+
+
+a = 3
+b = 6
+#lebih dari
+
+hasil = a > b
+print (a, '>', b, '=', hasil)
+
+#kurang dari
+hasil = a < b
+print (a, '>', b, '=', hasil)
+
+# lebih dari sama dengan
+hasil = a >= b
+print (a, '>=', b, '=', hasil)
+
+#kurang dari sama dengan
+hasil = a <= b
+print (a, '<=', b, '=', hasil)
+
+#is
+
+hasil = a is 3
+print (a, 'is', 3, '=', hasil)
+
+#is not
+hasil = a is not 4
+print (a, 'is not', 4, '=', hasil)
