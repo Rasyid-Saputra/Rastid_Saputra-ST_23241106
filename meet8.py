6ad56bf10edf6c77ca4dccdbe4468185b59a4d75
Rasyid-Saputra <147676432+Rasyid-Saputra@users.noreply.github.com>
2024-06-14 07:14:49 +0800

    Add files via upload

---
+++ meet8.py
@@ -0,0 +1,29 @@
+#percabangan jika mak
+
+nama = input("silakan masukan nama :")
+#if kodisi : aksi
+#if nama == "mugiwara" :print("ganteng banget")
+#print("program selesai")
+
+#if indentitas
+#if nama=="mugiwara":
+   # print("Mahasiswa ganteng")
+   # print("Mahasiswa banget")
+#print("program selesai")
+#print(f"thaks {nama}")
+
+
+ #if indentitas
+if nama=="mugiwara":
+    print("Mahasiswa ganteng")
+    print("Mahasiswa banget")
+else :
+    print("Mahasiswa kurang ganteng")
+    print("Mahasiswa kurang banget")
+    
+print("program selesai")
+print(f"thaks {nama}")
+
+
+
+
